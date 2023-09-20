bfd5b3104945d37de2ebfb6d794d6c360d8af50e
atikurahad <atikurahad@gmail.com>
2023-09-20 21:54:30 +0600

    api created

--- src/controllers/myController.js
+++ src/controllers/myController.js
@@ -1,4 +1,7 @@
 
-exports.hello = (req,res)=>{
-   res.status(200).json({status:"success", data:"everything is working"});
+exports.helloGet = (req,res)=>{
+   res.status(200).json({status:"success", data:"i am from get"});
+}
+exports.helloPost = (req,res)=>{
+   res.status(201).json({status:"success", data:"i am from post"});
 }
--- src/routes/api.js
+++ src/routes/api.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 const myController = require('../controllers/myController')
 
-router.get("/hello-get",myController.hello)
-router.post("/hello-post",myController.hello)
+router.get("/hello-get",myController.helloGet)
+router.post("/hello-post",myController.helloPost)
 
 
 module.exports = router;
